1cc5cb912a977fd37e41c2e1b874f2cd357cd410
Kira Sutton <ks14311@ms.wasatch.edu>
2022-05-05 11:52:00 -0600

    Fixed?

---
+++ Assets/Scripts/Input/Controls.cs
@@ -0,0 +1,216 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Input/Controls.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @Controls : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @Controls()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""Controls"",
+    ""maps"": [
+        {
+            ""name"": ""Locomotion"",
+            ""id"": ""735d8145-2689-43bd-8460-6df9e6ded16d"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""29defe39-6c6f-4299-a8ca-62b5b27558b2"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Look"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""60f96944-7bdb-45c1-982d-14e975f040e8"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""dfa94870-c547-4cc5-8b6b-7abc7ed19d91"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""12357c20-002a-4b97-9b74-8e15f2ee29e8"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""e625f1e2-fad2-45ca-89fb-1eb6c1f585b2"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""aac17401-4b54-402e-a07c-477eb2763ebf"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""e2a8fada-ed24-47d1-bcaa-d343274d83b7"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""b48f530d-cd57-4321-97ec-43db1b5034b7"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""58aa6c12-e3bc-45f7-8e5e-daac14201881"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": ""NormalizeVector2"",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Locomotion
+        m_Locomotion = asset.FindActionMap("Locomotion", throwIfNotFound: true);
+        m_Locomotion_Move = m_Locomotion.FindAction("Move", throwIfNotFound: true);
+        m_Locomotion_Look = m_Locomotion.FindAction("Look", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Locomotion
+    private readonly InputActionMap m_Locomotion;
+    private ILocomotionActions m_LocomotionActionsCallbackInterface;
+    private readonly InputAction m_Locomotion_Move;
+    private readonly InputAction m_Locomotion_Look;
+    public struct LocomotionActions
+    {
+        private @Controls m_Wrapper;
+        public LocomotionActions(@Controls wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_Locomotion_Move;
+        public InputAction @Look => m_Wrapper.m_Locomotion_Look;
+        public InputActionMap Get() { return m_Wrapper.m_Locomotion; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(LocomotionActions set) { return set.Get(); }
+        public void SetCallbacks(ILocomotionActions instance)
+        {
+            if (m_Wrapper.m_LocomotionActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnMove;
+                @Look.started -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnLook;
+                @Look.performed -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnLook;
+                @Look.canceled -= m_Wrapper.m_LocomotionActionsCallbackInterface.OnLook;
+            }
+            m_Wrapper.m_LocomotionActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Look.started += instance.OnLook;
+                @Look.performed += instance.OnLook;
+                @Look.canceled += instance.OnLook;
+            }
+        }
+    }
+    public LocomotionActions @Locomotion => new LocomotionActions(this);
+    public interface ILocomotionActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnLook(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private InputManager input;
+
+    public Transform targetTransform; //where the camera will eventually move to
+    public Transform camTransform; //location of the actual camera
+    public Transform pivotTransform; //location of the camera pivot
+
+    private Vector3 camPos; //the position data of the camera transform. Only matters for z position.
+    public LayerMask ignoreLayers;
+
+    private Vector3 cameraFollowVelocity = Vector3.zero;
+
+    public float lookSpeed = .03f;
+    public float followSpeed = .1f;
+    public float pivotSpeed = .03f;
+
+    public float targetPos;
+    private float defaultPos;
+    private float lookAngle;
+    private float pivotAngle;
+
+    public float minPivot = -35;
+    public float maxPivot = 35;
+
+    public float cameraSphereRadius = .2f;
+    public float cameraCollisionOffset = .2f;
+    public float minCollisionOffset = .2f;
+
+
+    private void Awake()
+    {
+        defaultPos = camTransform.localPosition.z;
+    }
+
+    void Start()
+    {
+        input = InputManager.instance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowTarget(Time.deltaTime);
+        HandleCameraRotation(Time.deltaTime);
+        
+    }
+
+    /// <summary>
+    /// Moves the transform of the camera holder gameobject towards the player's location.
+    /// </summary>
+    /// <param name="delta"></param>
+    private void FollowTarget(float delta)
+    {
+        //create a position somewhere between the current position and target position
+        Vector3 targetPosition = Vector3.SmoothDamp(transform.position, targetTransform.position, ref cameraFollowVelocity, delta / followSpeed);
+
+        //assign the value of targetPosition to the main transform.
+        transform.position = targetPosition;
+
+        HandleCameraCollision(delta);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="delta"></param>
+    private void HandleCameraRotation(float delta)
+    {
+        float mouseX = input.look.x;
+        float mouseY = input.look.y;
+
+        //calculate 'looking' AKA side to side turning
+        lookAngle += (mouseX * lookSpeed) / delta;
+
+        //calculate 'pivot' AKA up and down tilting
+        pivotAngle -= (mouseY * pivotSpeed) / delta;
+
+        //clamp the pivot so we can't tilt too far
+        pivotAngle = Mathf.Clamp(pivotAngle, minPivot, maxPivot);
+
+        //THE FOLLOWING SECTION APPLIES SIDE TO SIDE ROTATION:
+
+        //create a vector to store a new rotation into (we must initialize it or Quaternion.Euler() will complain)
+        Vector3 rotation = Vector3.zero;
+
+        //What would the y value of our new rotation be? Thats right. The look angle we calculated
+        rotation.y = lookAngle;
+
+        //make a quaternion out of the euler angle
+        Quaternion targetRotation = Quaternion.Euler(rotation);
+
+        transform.rotation = targetRotation;
+
+        //THE FOLLOWING SECTION APPLIES UP AND DOWN ROTATION:
+
+        //set the rotation back to zero
+        rotation = Vector3.zero;
+
+        //we want to tilt this pivot. So we affect the x angle.
+        rotation.x = pivotAngle;
+
+        targetRotation = Quaternion.Euler(rotation);
+
+        //why local rotation? See below. 
+        pivotTransform.localRotation = targetRotation;
+
+        /*
+         * When we are 'looking' we are turning the entire camera holder. 
+         * When we are 'tilting' we are tilting the CAMERA PIVOT *RELATIVE* to the camera holder.
+         * To do that, we have to use localRotation. 
+         */
+    }
+
+    private void HandleCameraCollision(float delta)
+    {
+        //the targetPos is the location (on the z) we want to move the camera to.
+        //by default every frame, the target z position is set to the default. So it is trying to reset itself every frame. 
+        targetPos = defaultPos;
+
+        //create a raycast hit to store hit data
+        RaycastHit hit;
+
+        //calculate the direction we want to cast in (from the camera position to the pivot)
+        Vector3 direction = camTransform.position - pivotTransform.position;
+        direction.Normalize();
+
+        //cast a sphere (of cameraSphereRadius size) from the pivot in the direction calculated earlier. 
+        //the furthest we need to cast is the absolute value of the farthest the camera can possibly go.
+        if(Physics.SphereCast(pivotTransform.position, cameraSphereRadius, direction, out hit, Mathf.Abs(targetPos), ignoreLayers))
+        {
+            //if we hit something, find out how far it was from the pivot to that point. 
+            float dis = Vector3.Distance(pivotTransform.position, hit.point);
+
+            //update the target position with this distance made negative
+            targetPos = -(dis - cameraCollisionOffset);
+
+            //if our new target position is less than our minimum offset, update to the minimum collision offset.
+            //This will keep the camera from going inside the player.
+            if (Mathf.Abs(targetPos) < minCollisionOffset)
+            {
+                targetPos = -minCollisionOffset;
+            }
+        }
+
+        //calculate a new position between the camera current position and the target position
+        camPos.z = Mathf.Lerp(camTransform.localPosition.z, targetPos, delta / .2f);
+        //move the actual camera position to the calculated position.
+        camTransform.localPosition = camPos;
+
+        //NOTE: Since we know that the cameraTransform x and y values are zero, setting them equal to camPos works. 
+        //If for some reason they were not zero, we would need to update camPos. 
+
+        //NOTE: Why do we keep targetPos negative? Because we want the camera to be behind the cameraPivot. 
+        //When there is a successful spherecast, we are not increasing the distance between the pivot and the camera, we are decreasing it. 
+        //So when a collision occurs, targetPos will be set to a smaller negative number (in terms of absolute value) than it was before. 
+    }
+}
